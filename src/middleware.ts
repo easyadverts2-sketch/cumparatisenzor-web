fe9922cbf93aa8b5fd6bf56535dc9725ba0c0b3b
easyadverts2-sketch <easyadverts2@gmail.com>
2026-04-28 14:25:13 +0200

    pictures fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -74,6 +74,7 @@ export async function middleware(request: NextRequest) {
       pathname.startsWith("/_next") ||
       pathname.startsWith("/favicon") ||
       pathname.startsWith("/icon") ||
+      pathname.startsWith("/app-assets") ||
       pathname.startsWith("/sensor-motif") ||
       pathname.startsWith("/brand-logo") ||
       pathname.startsWith("/libre-");
